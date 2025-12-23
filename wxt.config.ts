9e55045832662d36757556bbe69f943051bbb331
ABSanthosh <a.b.santhosh02@gmail.com>
2025-12-23 16:35:08 -0500

    init: assets and setup

---
+++ wxt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  srcDir: 'src',
+  modules: ['@wxt-dev/module-svelte'],
+  manifest: {
+    name: process.env.NODE_ENV === "production" ? "Ara" : "Ara Dev",
+  }
+});
